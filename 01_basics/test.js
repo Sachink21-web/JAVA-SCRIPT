436f02ef6bafad9eabcb10df114db1176949b3a7
Sachink21-web <sachin678july@gmail.com>
2026-09-16 05:50:26 +0000

    variables tak

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,8 @@
-console.log("RAM RAM")
+slogn = "Ram Ram"
+const appleID = 123456
+var acountEmail = "sachin@google.com"
+SchoolName = "KENdriya vidyalaya no.2"
+let yesorno = null
+console.table([appleID, acountEmail, slogn , SchoolName])
+console.log(3 + 3);
+ console.table([typeof appleID, typeof acountEmail, typeof slogn, typeof SchoolName , typeof  yesorno]);
