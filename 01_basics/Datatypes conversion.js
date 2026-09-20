0616ab76f3791cd9a0975c3d00f29e9b4f82c61c
Sachink21-web <sachin678july@gmail.com>
2026-09-20 15:41:48 +0000

    DATA CONVERSION

---
+++ 01_basics/Datatypes conversion.js
@@ -0,0 +1,40 @@
+let state = null
+console.log(state)
+console.log(2>1)
+
+let rollno = "IT113"
+console.log(typeof rollno);
+
+let newrollno = Number(rollno)
+console.log(typeof newrollno)
+console.log(newrollno)
+
+/*
+DATA TYPE CONVERSION
+"113" easily convert ho jata hee 33 numbeer mee iska type Number hee
+"IT113" number mee convertkrne mee NaN aata hee but iska type Number hii hee
+boolean mee "true" ya "false" convert krne mee 1 ya 0 aata hee
+*/
+
+
+let Studentispresent = "meee"
+console.log(Studentispresent)
+
+let checkStudentispresent = Boolean(Studentispresent)
+console.log(checkStudentispresent)
+/*
+DATA TYPE CONVERSION
+"meee" boolean mee convert krne mee true aata hee
+"" boolean mee convert krne mee false aata hee
+*/
+
+let anynumber = 123
+console.log(anynumber)
+
+let stringnumber = String(anynumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
+/*
+DATA TYPE CONVERSION
+123 string mee convert krne mee "123" aata hee
+*/
