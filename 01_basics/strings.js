0575f063db447fc79856c4fe7971c5bec30e523a
Sachink21-web <sachin678july@gmail.com>
2026-09-22 19:05:21 +0000

    lecture 11 complete

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -1,25 +1,53 @@
-let stefan = 'love elena'
-console.log(stefan)
+// let stefan = 'love elena'
+// console.log(stefan)
 
-const originalvampire = 'klaus mikelson'
-let age = 1086
-// console.log(vampire + age + "most strongest")
+// const originalvampire = 'klaus mikelson'
+// let age = 1086
+// // console.log(vampire + age + "most strongest")
 
-console.log(`hello love i am the hybrid ${originalvampire} and i am ${age} years old.........`)
-// this is the moder way of string representation 
+// console.log(`hello love i am the hybrid ${originalvampire} and i am ${age} years old.........`)
+// // this is the moder way of string representation 
 
-const newVamp = new String('DEMON')
-console.log(newVamp)
-// this is another way of string ddeclaration 
+// const newVamp = new String('DEMON')
+// console.log(newVamp)
+// // this is another way of string ddeclaration 
 
-console.log(newVamp.length)
-console.log(newVamp.__proto__)
-console.log(newVamp.toLowerCase())
-console.log(newVamp.charAt(4))
-console.log(newVamp.indexOf('M'))
+// console.log(newVamp.length)
+// console.log(newVamp.__proto__)
+// console.log(newVamp.toLowerCase())
+// console.log(newVamp.charAt(4))
+// console.log(newVamp.indexOf('M'))
 
-const oldvamp = newVamp.substring(0 , 3)
-console.log(oldvamp)
+// const oldvamp = newVamp.substring(0 , 3)
+// console.log(oldvamp)
 
-const anothervamp = newVamp.slice(-4 , -2)
-console.log(anothervamp)
+// const anothervamp = newVamp.slice(-4 , -2)
+// console.log(anothervamp)
+
+// const newVampire = '  ELIJAH  '
+// console.log(newVampire)
+// console.log(newVampire.trim())
+
+// const url = 'https://vampiresdiaries$.in'
+// console.log(url.replace('$' , '-'))
+
+// console.log(url.includes('vampires'))
+// console.log(url.includes('rebekahh'))
+
+const elijah = "the noble original vampire always wears a suit"
+const elijah2 = "the-noble-original-vampire-always-wears-a-suit"
+
+// const output = elijah.split(" ")
+// console.log(output[2])
+
+const output2 = elijah.split('')
+ console.log(output2[8])
+
+ const output3 = elijah.split()
+ console.log(output3)
+
+ const output4 = elijah2.split('-')
+ console.table([output4])
+
+ const NOBLE = 'ELIJAH'
+console.log(NOBLE.fontsize('7pt'))
