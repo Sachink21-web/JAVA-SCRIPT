3340da95dc23b29adcc9b872d99bb83644b75530
Sachink21-web <sachin678july@gmail.com>
2026-09-22 11:17:03 +0000

    lecture 11 tkk

---
+++ 01_basics/strings.js
@@ -0,0 +1,12 @@
+let stefan = 'love elena'
+console.log(stefan)
+
+const originalvampire = 'klaus mikelson'
+let age = 1086
+// console.log(vampire + age + "most strongest")
+
+console.log(`hello love i am the hybrid ${originalvampire} and i am ${age} years old.........`)
+// this is the moder way of string representation 
+
+const newVamp = new String('DEMON')
+// this is another way of string ddeclaration 
