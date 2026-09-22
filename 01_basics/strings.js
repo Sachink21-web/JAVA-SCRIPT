05cd38f080ec08c75775606bcc858175d152f19b
Sachink21-web <sachin678july@gmail.com>
2026-09-22 12:28:38 +0000

    11 still running

--- 01_basics/strings.js
+++ 01_basics/strings.js
@@ -9,4 +9,17 @@ console.log(`hello love i am the hybrid ${originalvampire} and i am ${age} years
 // this is the moder way of string representation 
 
 const newVamp = new String('DEMON')
+console.log(newVamp)
 // this is another way of string ddeclaration 
+
+console.log(newVamp.length)
+console.log(newVamp.__proto__)
+console.log(newVamp.toLowerCase())
+console.log(newVamp.charAt(4))
+console.log(newVamp.indexOf('M'))
+
+const oldvamp = newVamp.substring(0 , 3)
+console.log(oldvamp)
+
+const anothervamp = newVamp.slice(-4 , -2)
+console.log(anothervamp)
